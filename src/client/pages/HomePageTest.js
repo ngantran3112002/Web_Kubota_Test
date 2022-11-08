545e33efb4f12e8b70fcde1dec149f263a832212
Tuan Anh Phung <tuananh.phung@dssolution.jp>
2022-11-08 17:48:19 +0700

    update taskbar

--- src/client/pages/HomePageTest.js
+++ src/client/pages/HomePageTest.js
@@ -2,9 +2,7 @@ import { faLocation, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { makeStyles } from "@mui/styles";
 import React from "react";
-import {
-  Route, Routes, useNavigate
-} from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import logo from "../../image/logo.png";
 import HomePage from "./homePage";
 import Nav from "./Nav";
@@ -58,50 +56,48 @@ const HomePageTest = () => {
     navigate("/cart", { replace: true });
   };
   return (
-      <div className={classes.container}>
-        <div className={classes.header}>
-          <div className={classes.logo}>
-            <span>
-              <img src={logo} alt="company logo" style={{ width: "70%" }} />
-            </span>
-          </div>
-          <div className={classes.info}>
-            <div className={classes.contact}>
-              <div className={classes.contactLeft}>
-                <FontAwesomeIcon icon={faPhone} /> HOTLINE: 0398.490.986 -
-                0949.265.919
-              </div>
-              <div className={classes.contactRight}>
-                <FontAwesomeIcon icon={faLocation} />
-                <a href="https://g.page/KubotaBMP?share">
-                  PHỤ TÙNG MÁY NÔNG NGHIỆP KUBOTA
-                </a>
-              </div>
-            </div>
-            <div className={classes.taskbar}>
-              <div className={classes.leftTaskbar}>
-                <div className={classes.navTop}>
-                  <Nav />
-                </div>
-                <div className={classes.rightTaskbar}>
-                  <button onClick={handleCartButton}>GIỎ HÀNG</button>
-                  <button onClick={handleLoginButton}>ĐĂNG NHẬP</button>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className={classes.container}>
+      <div className={classes.header}>
+        <div className={classes.logo}>
+          <span>
+            <img src={logo} alt="company logo" style={{ width: "70%" }} />
+          </span>
         </div>
-
-        <div className={classes.content}>
-          <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route exact path="/product" element={<Product />} />
-            <Route exact path="/introduction" element={<HomePage />} />
-            <Route exact path="/login" element={<Product />} />
-            <Route exact path="/cart" element={<HomePage />} />
-          </Routes>
+        <div className={classes.info}>
+          <div className={classes.contact}>
+            <div className={classes.contactLeft}>
+              <FontAwesomeIcon icon={faPhone} /> HOTLINE: 0398.490.986 -
+              0949.265.919
+            </div>
+            <div className={classes.contactRight}>
+              <FontAwesomeIcon icon={faLocation} />
+              <a href="https://g.page/KubotaBMP?share">
+                PHỤ TÙNG MÁY NÔNG NGHIỆP KUBOTA
+              </a>
+            </div>
+          </div>
+          <div className={classes.taskbar}>
+            <div className={classes.navTop}>
+              <Nav />
+            </div>
+            <div className={classes.rightTaskbar}>
+              <button onClick={handleCartButton}>GIỎ HÀNG</button>
+              <button onClick={handleLoginButton}>ĐĂNG NHẬP</button>
+            </div>
+          </div>
         </div>
       </div>
+
+      <div className={classes.content}>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/product" element={<Product />} />
+          <Route exact path="/introduction" element={<HomePage />} />
+          <Route exact path="/login" element={<Product />} />
+          <Route exact path="/cart" element={<HomePage />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
