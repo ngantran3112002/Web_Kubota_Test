3557680832bf32f0492b9f9f75700301022f8e5c
Le Phuong Uyen <uyentk0902@gmail.com>
2022-11-23 19:14:36 +0700

    update about

--- src/client/pages/about.js
+++ src/client/pages/about.js
@@ -10,48 +10,50 @@ const About = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-4">
-                            <h2 className="header-style">Về Kubota</h2>
-                            <p><strong> &nbsp; Th&aacute;ng 4 năm 2018 website: phutungkubota.vn được tạo ra c&ugrave;ng&nbsp;c&aacute;c t&ecirc;n miền đầy đủ: phutungkubota.com v&agrave; phutungkubota.com.vn.</strong></p>
+                            <h2 className="header-style tapered">Về Kubota</h2>
+                            <p className="p-style"> &nbsp; Th&aacute;ng 4 năm 2018 website: phutungkubota.vn được tạo ra c&ugrave;ng&nbsp;c&aacute;c t&ecirc;n miền đầy đủ: phutungkubota.com v&agrave; phutungkubota.com.vn.</p>
 
-                            <p><strong> &nbsp; Với kỳ vọng gi&uacute;p kh&aacute;ch h&agrave;ng sử dụng m&aacute;y n&ocirc;ng nghiệp mua được phụ t&ugrave;ng ch&iacute;nh h&atilde;ng từ Kubota Việt nam với những thao t&aacute;c đơn giản tr&ecirc;n điện thoại, m&aacute;y t&iacute;nh, hoặc Tivi c&oacute; kết nối internet...&nbsp;</strong></p>
+                            <p className="p-style"> &nbsp; Với kỳ vọng gi&uacute;p kh&aacute;ch h&agrave;ng sử dụng m&aacute;y n&ocirc;ng nghiệp mua được phụ t&ugrave;ng ch&iacute;nh h&atilde;ng từ Kubota Việt nam với những thao t&aacute;c đơn giản tr&ecirc;n điện thoại, m&aacute;y t&iacute;nh, hoặc Tivi c&oacute; kết nối internet...&nbsp;</p>
 
-                            <p><strong> &nbsp; Qu&yacute; kh&aacute;ch h&agrave;ng sẽ nhận được phụ t&ugrave;ng tại nh&agrave; &gt;&gt;&gt; kiểm tra h&agrave;ng h&oacute;a &gt;&gt;&gt; thanh to&aacute;n cho nh&acirc;n vi&ecirc;n chuyển ph&aacute;t. Kh&ocirc;ng phải mất c&ocirc;ng t&igrave;m kiếm! Kh&ocirc;ng mất thời gian c&ocirc;ng sức di chuyển! Mua được h&agrave;ng ch&iacute;nh h&atilde;ng Kubota! Tiết kiệm thời gian để vận h&agrave;nh v&agrave; sử dụng m&aacute;y m&oacute;c n&acirc;ng cao năng xuất lao động!</strong></p>
+                            <p className="p-style"> &nbsp; Qu&yacute; kh&aacute;ch h&agrave;ng sẽ nhận được phụ t&ugrave;ng tại nh&agrave; &gt;&gt;&gt; kiểm tra h&agrave;ng h&oacute;a &gt;&gt;&gt; thanh to&aacute;n cho nh&acirc;n vi&ecirc;n chuyển ph&aacute;t. Kh&ocirc;ng phải mất c&ocirc;ng t&igrave;m kiếm! Kh&ocirc;ng mất thời gian c&ocirc;ng sức di chuyển! Mua được h&agrave;ng ch&iacute;nh h&atilde;ng Kubota! Tiết kiệm thời gian để vận h&agrave;nh v&agrave; sử dụng m&aacute;y m&oacute;c n&acirc;ng cao năng xuất lao động!</p>
 
-                            <p><strong> &nbsp; B&ecirc;n cạnh đ&oacute; website l&agrave; 1 kho dữ liệu sơ đồ th&aacute;o lắp m&aacute;y Kubota ch&iacute;nh x&aacute;c v&agrave;&nbsp;đầy đủ d&ograve;ng m&aacute;y, được c&ocirc;ng ty Kubota bảo hộ kỹ thuật tại nước Việt Nam.&nbsp;</strong></p>
+                            <p className="p-style"> &nbsp; B&ecirc;n cạnh đ&oacute; website l&agrave; 1 kho dữ liệu sơ đồ th&aacute;o lắp m&aacute;y Kubota ch&iacute;nh x&aacute;c v&agrave;&nbsp;đầy đủ d&ograve;ng m&aacute;y, được c&ocirc;ng ty Kubota bảo hộ kỹ thuật tại nước Việt Nam.&nbsp;</p>
                         </div>
                         
                         <div className="col-lg-4">
-                            <h2 className="header-style">Các kho hàng</h2>
-                            <p><strong> &nbsp; Hiện tại ch&uacute;ng t&ocirc;i giao h&agrave;ng phụ t&ugrave;ng cho qu&yacute; kh&aacute;ch h&agrave;ng trực tiếp từ 1 trong 3 kho h&agrave;ng tại Bắc - Trung - Nam.</strong></p>
+                            <h2 className="header-style tapered">Các kho hàng</h2>
+                            <p className="p-style"> &nbsp; Hiện tại ch&uacute;ng t&ocirc;i giao h&agrave;ng phụ t&ugrave;ng cho qu&yacute; kh&aacute;ch h&agrave;ng trực tiếp từ 1 trong 3 kho h&agrave;ng tại Bắc - Trung - Nam.</p>
 
-                            <p><strong> 1. B&ecirc;n C&aacute;t - B&igrave;nh Dương. - Kubota Việt Nam</strong></p>
+                            <p className="p-style"> 1. B&ecirc;n C&aacute;t - B&igrave;nh Dương. - Kubota Việt Nam</p>
 
-                            <p><strong> 2. Từ Sơn - Bắc Ninh. - Kubota Việt Nam</strong></p>
+                            <p className="p-style"> 2. Từ Sơn - Bắc Ninh. - Kubota Việt Nam</p>
 
-                            <p><strong> 3. Văn ph&ograve;ng đại diện KTM tại&nbsp;Y&ecirc;n Đinh - Thanh H&oacute;a.</strong></p>
+                            <p className="p-style"> 3. Văn ph&ograve;ng đại diện KTM tại&nbsp;Y&ecirc;n Đinh - Thanh H&oacute;a.</p>
 
-                            <p><strong> 4. Văn ph&ograve;ng đại diện KTM&nbsp; tại ICID Complex L&ecirc; Trọng Tấn - H&agrave; Đ&ocirc;ng - H&agrave; Nội</strong></p>
+                            <p className="p-style"> 4. Văn ph&ograve;ng đại diện KTM&nbsp; tại ICID Complex L&ecirc; Trọng Tấn - H&agrave; Đ&ocirc;ng - H&agrave; Nội</p>
 
-                            <p><strong> &nbsp; Ch&uacute;ng t&ocirc;i&nbsp;tiếp tục mở rộng th&ecirc;m c&aacute;c kho h&agrave;ng tại c&aacute;c đại l&yacute; ch&iacute;nh h&atilde;ng của Kubota Việt Nam, để giảm thời gian v&agrave; chi ph&iacute; vận chuyển cho qu&yacute; kh&aacute;ch h&agrave;ng trong thời gian tới.&nbsp;</strong></p>
+                            <p className="p-style"> &nbsp; Ch&uacute;ng t&ocirc;i&nbsp;tiếp tục mở rộng th&ecirc;m c&aacute;c kho h&agrave;ng tại c&aacute;c đại l&yacute; ch&iacute;nh h&atilde;ng của Kubota Việt Nam, để giảm thời gian v&agrave; chi ph&iacute; vận chuyển cho qu&yacute; kh&aacute;ch h&agrave;ng trong thời gian tới.&nbsp;</p>
 
-                            <p><strong> &nbsp; Với lượng tồn kho lớn v&agrave; kho h&agrave;ng tr&ecirc;n khắp 3 miền đất nước, ch&uacute;ng t&ocirc;i mang đến cho qu&yacute; kh&aacute;ch h&agrave;ng sự nhanh ch&oacute;ng v&agrave; tin cậy.&nbsp;</strong></p>
+                            <p className="p-style"> &nbsp; Với lượng tồn kho lớn v&agrave; kho h&agrave;ng tr&ecirc;n khắp 3 miền đất nước, ch&uacute;ng t&ocirc;i mang đến cho qu&yacute; kh&aacute;ch h&agrave;ng sự nhanh ch&oacute;ng v&agrave; tin cậy.&nbsp;</p>
                         </div>
                         
                         <div className="col-lg-4">
                             <h2 className="header-style">Chọn Kubota vì</h2>
-                            <p><strong> &nbsp; Sản phẩm của ch&uacute;ng t&ocirc;i l&agrave;: 100% h&agrave;ng phụ t&ugrave;ng l&agrave; h&agrave;ng ch&iacute;nh h&atilde;ng kubota để đảm bảo chất lượng v&agrave; nhận diện h&agrave;ng h&oacute;a cho người sử dụng.&nbsp;</strong></p>
+                            
+                            <p className="p-style"> &nbsp; Sản phẩm của ch&uacute;ng t&ocirc;i l&agrave;: 100% h&agrave;ng phụ t&ugrave;ng l&agrave; h&agrave;ng ch&iacute;nh h&atilde;ng kubota để đảm bảo chất lượng v&agrave; nhận diện h&agrave;ng h&oacute;a cho người sử dụng.&nbsp;</p>
 
-                            <p><strong> &nbsp; Phutungkubota.vn kho phụ t&ugrave;ng tr&ecirc;n tay của bạn!</strong></p>
+                            <p className="p-style"> &nbsp; H&atilde;y đặt mua h&agrave;ng tại website để nhận được phụ t&ugrave;ng ch&iacute;nh h&atilde;ng v&agrave; thanh to&aacute;n tại nh&agrave; khi nhận h&agrave;ng. Ch&uacute;ng t&ocirc;i giao h&agrave;ng đến bất cứ nơi đ&acirc;u bạn y&ecirc;u cầu!</p>
 
-                            <p><strong> &nbsp; H&atilde;y đặt mua h&agrave;ng tại website để nhận được phụ t&ugrave;ng ch&iacute;nh h&atilde;ng v&agrave; thanh to&aacute;n tại nh&agrave; khi nhận h&agrave;ng. Ch&uacute;ng t&ocirc;i giao h&agrave;ng đến bất cứ nơi đ&acirc;u bạn y&ecirc;u cầu!</strong></p>
+                            <p className="p-style"> &nbsp; H&atilde;y v&agrave;o website để t&igrave;m hiểu th&ocirc;ng tin sơ đồ th&aacute;o lắp m&aacute;y Kubota ti&ecirc;u chuẩn của tập đo&agrave;n Kubota Nhật bản!&nbsp;</p>
 
-                            <p><strong> &nbsp; H&atilde;y v&agrave;o website để t&igrave;m hiểu th&ocirc;ng tin sơ đồ th&aacute;o lắp m&aacute;y Kubota ti&ecirc;u chuẩn của tập đo&agrave;n Kubota Nhật bản!&nbsp;</strong></p>
+                            <p className="p-style"> &nbsp; Với những t&agrave;i liệu sơ đồ th&aacute;o lắp tr&ecirc;n website gi&uacute;p bạn tự tin th&aacute;o lắp ch&iacute;nh x&aacute;c m&aacute;y m&oacute;c của bạn!&nbsp;</p>
 
-                            <p><strong> &nbsp; Với những t&agrave;i liệu sơ đồ th&aacute;o lắp tr&ecirc;n website gi&uacute;p bạn tự tin th&aacute;o lắp ch&iacute;nh x&aacute;c m&aacute;y m&oacute;c của bạn!&nbsp;</strong></p>
+                            <p className="p-style"> &nbsp; Team Kỹ Thuật M&aacute;y N&ocirc;ng Nghiệp! Tr&acirc;n trọng cảm ơn qu&yacute; kh&aacute;ch h&agrave;ng b&agrave; con đ&atilde; trao gửi niềm tin về sản phẩm tr&ecirc;n phutungkubota.vn! </p>
 
-                            <p><strong> &nbsp; Team Kỹ Thuật M&aacute;y N&ocirc;ng Nghiệp! Tr&acirc;n trọng cảm ơn qu&yacute; kh&aacute;ch h&agrave;ng b&agrave; con đ&atilde; trao gửi niềm tin về sản phẩm tr&ecirc;n phutungkubota.vn! </strong></p>
+                            <p className="p-style"> &nbsp; Ch&uacute;ng t&ocirc;i lu&ocirc;n l&agrave;m việc tận t&igrave;nh&nbsp;để đưa đến qu&yacute; b&agrave; con những sản phẩm tốt nhất với gi&aacute; th&agrave;nh rẻ nhất!&nbsp;</p>
+                        
+                            <p className="p-style"> &nbsp; Phutungkubota.vn kho phụ t&ugrave;ng tr&ecirc;n tay của bạn!</p>
 
-                            <p><strong> &nbsp; Ch&uacute;ng t&ocirc;i lu&ocirc;n l&agrave;m việc tận t&igrave;nh&nbsp;để đưa đến qu&yacute; b&agrave; con những sản phẩm tốt nhất với gi&aacute; th&agrave;nh rẻ nhất!&nbsp;</strong></p>
                         </div>
                         
                     </div>
