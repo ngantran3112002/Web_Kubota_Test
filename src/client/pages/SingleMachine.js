9612cc1570d36a9834eff3ad2268b34e813e309e
Le Phuong Uyen <uyentk0902@gmail.com>
2022-11-23 18:12:39 +0700

    update about

---
+++ src/client/pages/SingleMachine.js
@@ -0,0 +1,56 @@
+import { Link, useNavigate , useParams, useLocation } from 'react-router-dom'
+import machineData from './machineData';
+import "../css/singleMachine.css";
+ 
+
+const SingleMachine = () => {
+
+  const navigate = useNavigate();
+  const { machineId } = useParams();
+  //const { pathname } = useLocation();
+
+  // get machine
+  const singleMachine = machineData.find((machine) => machine.id === machineId);
+  
+  // console.log(typeof(machineId))
+
+  const { id, name, desciption1, desciption2, desciption3, image1, image2, image3, image4, image5, details1 } = singleMachine;
+
+  // console.log("machine id ", machineId)
+  // console.log("machine ", SingleMachine)
+
+  return (
+    <section>
+        <div className="pg-header">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div>
+                        <h2>{name}</h2>
+                        {/* <p>{pathname}</p> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="container content">
+            <div className="row">
+                <div>
+                    <h5>{desciption1}</h5>
+                    <img src={image2} alt="" className="img-fluid" />
+                    <h5>{desciption2}</h5>
+                    <img src={image3} alt="" className="img-fluid" />
+                    <h5>{desciption3}</h5>
+                    <img src={image4} alt="" className="img-fluid" />
+                    <img src={image5} alt="" className="img-fluid" />
+                </div>
+                <div>
+                    <br/>
+                    <button className="btn btn-primary btn-xl" onClick={() => navigate(-1)}> &larr; Back </button> &nbsp; 
+                    <Link to="/machine" className="btn btn-primary btn-xl"> Kĩ thuật máy &rarr; </Link>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default SingleMachine;
